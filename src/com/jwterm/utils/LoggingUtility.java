ead7d24fa1bce102690f120a2323754dff4ccf67
Pavel Pereverzev <gotoss08@gmail.com>
2025-04-07 00:07:02 +0500

    Move LogFormatter to LoggingUtility

--- src/com/jwterm/utils/LoggingUtility.java
+++ src/com/jwterm/utils/LoggingUtility.java
@@ -1,13 +1,28 @@
 package com.jwterm.utils;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.FileHandler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Date;
+import java.util.logging.*;
 
 public class LoggingUtility {
+
+    public static class LogFormatter extends Formatter {
+        @Override
+        public String format(LogRecord record) {
+            // Create a simple date format
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String timestamp = dateFormat.format(new Date(record.getMillis()));
+
+            // Format the log message in standard format
+            String level = record.getLevel().getName();
+            String className = record.getSourceClassName();
+            String message = record.getMessage();
+
+            return String.format("[%s] [%s] [%s] - %s%n", timestamp, level, className, message);
+        }
+    }
 
     public static ArrayList<Logger> loggers = new ArrayList<>();
 
